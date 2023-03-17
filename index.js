0ec11a907f3c8bf189e992f7ac1c203757564756
Ulises <ulises.argueta95@gmail.com>
2023-03-17 17:38:51 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 14;
+const num4 = 5;
 
-// Write your code here
+const multiply = num1*num2;
+
+const mod = num3 % num4
+
+const max = Math.max(2, 6, 4, 14, 20, 16)
+
+const random = Math.floor(Math.random() *10) + 1;
+Math.isinteger(random);
+
+
+
